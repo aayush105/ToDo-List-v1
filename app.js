2a2ed1431b9d6d4fdb68b2b5acb04c45dc1269c5
Aayush shrestha <aayushrestha5@gmail.com>
2023-05-25 21:54:08 +0545

    complete , now we can add item in todo list

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js"); // it require the date.js and gets the module and store into date 
+
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); // this helps us to use the css in our website 
 
 app.get("/", function(req, res){
 
-    let today = new Date();
-    
-    let option = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    
-    let day = today.toLocaleDateString("en-US", option); // this will generate the today's date with the respective weekday
-
-    res.render("list", {kindOfDay: day, newListItems: items}); // it will send the value of day to the list.ejs file in views folder so that it can be render in the template file
+    const day = date.getDate();
+    res.render("list", {listTitle: day, newListItems: items}); // it will send the value of day to the list.ejs file in views folder so that it can be render in the template file
 });
 
 app.post("/", function(req, res){
-    let item = req.body.newItem; // we will get the value from the input box from the template
+    const item = req.body.newItem; // we will get the value from the input box from the template
 
-    items.push(item);
-
-    res.redirect("/"); // and the it will redirect to the home route and get trigger the app.get in home route adn then render the both value in the templete
-
-    
+    if (req.body.list === "Work List"){
+        workItems.push(item);
+        res.redirect("/work"); // it will redirect to the work route and get trigger the app.get in work route and then render the value in the templete
+    } else {
+        items.push(item);
+        res.redirect("/"); // it will redirect to the home route and get trigger the app.get in home route and then render the both value in the templete
+    }
 });
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    const item= req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
 
 app.listen(3000, function(){
     console.log("Server started at port 3000");
